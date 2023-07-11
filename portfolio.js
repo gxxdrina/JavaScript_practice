8554a4779346b8e4a7700160da43679d7f267a10
RinaOta <127483650+gxxdrina@users.noreply.github.com>
2023-07-11 14:46:23 +0000

    practice JavaScript

--- portfolio.js
+++ portfolio.js
@@ -1,2 +1,34 @@
 // let hello = "Hi there I'm Rina";
 // alert(hello);
+
+// varによる再宣言はOK
+var nickname = "taro"
+console.log(nickname)
+var nickname = "ichiro"
+console.log(nickname)
+
+// varとletは再代入OK
+let name = "taro"
+console.log(name)
+name = "jiro"
+console.log(name)
+
+// 0~9までをループによってコンソールに出力
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//新たに値を代入する必要のないものに関してはconstで宣言
+
+
+//
+var str = "webcamp"
+
+function foo() { 
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
+
