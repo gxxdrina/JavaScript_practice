6e14c632d67c9a2ac579b9858dc98e6cc467dffe
RinaOta <127483650+gxxdrina@users.noreply.github.com>
2023-07-26 13:20:37 +0000

    Add Swiper

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -33,6 +33,12 @@
 // });
 
 $(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
+  });
+});
+
+$(function(){
   $('.box1').on('click', function(){
     $(this).slideUp();
   });
