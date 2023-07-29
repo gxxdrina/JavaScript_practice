a3ac6268f62c9e94cd06b48574752e5997a92b3c
RinaOta <127483650+gxxdrina@users.noreply.github.com>
2023-07-29 11:58:20 +0000

    Add hm-menu

--- jQuery-Lesson/js/script.js
+++ jQuery-Lesson/js/script.js
@@ -12,3 +12,14 @@ $(function() {
     event.preventDefault();
   });
 });
+
+
+
+$(function() {
+  $('.menu-trigger').on('click', function(event) {
+    // .menu-triggerクリック時に行われる処理
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
+  });
+});
